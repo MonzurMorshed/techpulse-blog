36c3df29989df4263b26527bf373f02c5bd4d306
MonzurMorshed <monzurmorshedcse@gmail.com>
2025-11-01 17:23:20 +0600

    prisma client issue solved.

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+
+import { PrismaClient } from "@prisma/client/extension";
 
 declare global {
     var prisma: PrismaClient | undefined;
